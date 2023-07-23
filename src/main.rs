e17b20e8d97fa0183028c4f2c546b4d998c3d5bd
Casey <leptone55@gmail.com>
2023-07-23 15:57:18 +0100

    verift

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 fn exponentiate(num: u32, exp: u32, p: u32) -> u32 {
     num.pow(exp) % p
 }
@@ -13,9 +15,23 @@ fn solve(x: u32, k: u32, c: u32, q: u32) -> u32 {
     }
 }
 
+fn verify(g: u32, h: u32, p: u32, y1: u32, y2: u32, r1: u32, r2: u32, c: u32, s: u32) -> bool {
+    // r1 = g ^ s * y1 ^ c
+    let eq1: bool = r1 == exponentiate(g, s, p) * exponentiate(y1, c, p) % p;
+    // r2 = h ^ s * y2 ^ c
+    let eq2: bool = r2 == exponentiate(h, s, p) * exponentiate(y2, c, p) % p;
+
+    eq1 && eq2
+}
+
+fn randome_number() -> u32 {
+    let mut rng = rand::thread_rng();
+
+    rng.gen()
+}
+
 fn main() {
-    println!("{}", 4 % 3);
-    println!("{}", -4 % 3);
+    println!("{}", randome_number());
 }
 
 #[cfg(test)]
@@ -36,5 +52,21 @@ mod tests {
         assert_eq!(solve(2, 10, 1, 101), 8);
         // (10 - 2 * 6) mod 101 = 99
         assert_eq!(solve(2, 10, 6, 101), 99);
+    }
+
+    #[test]
+    fn test_verify() {
+        let g = 4;
+        let h = 9;
+        let q: u32 = 11;
+        let p = 23;
+        let y1 = 2;
+        let y2 = 3;
+        let r1 = 8;
+        let r2 = 4;
+        let c = 4;
+        let s = 5;
+        assert!(verify(g, h, p, y1, y2, r1, r2, c, s));
+        assert!(!verify(g, h, p, y1, y2, r1, r2, c, (s + 1) % q));
     }
 }
